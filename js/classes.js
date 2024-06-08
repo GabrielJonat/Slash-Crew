9e8859f66f34aaff0b7833b879fe0fe85a595606
Gabriel Jonathan <gabrielpadawan912@gmail.com>
2024-06-08 14:31:34 -0300

    html changes

--- js/classes.js
+++ js/classes.js
@@ -30,7 +30,7 @@ class Sprite {
 
         c.drawImage(this.img, this.framesCurrent * (this.img.width / this.framesMax) , 0, this.img.width / this.framesMax, this.img.height, this.position.x - this.offset.x , this.position.y - this.offset.y ,  (this.img.width / this.framesMax) * this.scale, this.img.height  * this.scale)
         
-       // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+       //c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
     }
 
